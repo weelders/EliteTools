025f6305707cba9087647e9ba0a7f81a124d82bb
weelders <40818593+weelders@users.noreply.github.com>
2020-09-14 11:25:16 +0200

    Add Views + make intent onClick System + Add SystemActivity

---
+++ app/src/main/java/fr/weelders/elitetools/SystemActivity.kt
@@ -0,0 +1,11 @@
+package fr.weelders.elitetools
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SystemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_system)
+    }
+}
